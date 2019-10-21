30739967af2de01f60df4c3a53c8f37fddb7c7bc
Brandon Weaver <a7xncob@gmail.com>
2019-10-21 23:55:41 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,9 +2,12 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  pp directors_database
-  result = {
-  }
+  result = {}
+  
+  i = 0
+  while i < directors_database.size do
+    
+  end
   
   return result
 end
