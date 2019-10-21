54c653ecf70e56c83a58f293232a4568e9232b56
Brandon Weaver <a7xncob@gmail.com>
2019-10-21 23:53:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,14 +5,6 @@ def directors_totals(nds)
   pp directors_database
   result = {
   }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  
+  return result
 end
