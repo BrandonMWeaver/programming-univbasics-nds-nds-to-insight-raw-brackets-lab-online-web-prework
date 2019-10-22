91536eac3526c7d3f94161ef5cff32e29eafb78f
Brandon Weaver <a7xncob@gmail.com>
2019-10-22 00:00:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,9 +5,9 @@ def directors_totals(nds)
   result = {}
   
   i = 0
-  while i < directors_database.size do
+  while i < directors_database[:name].size do
     total = 0
-    puts directors_database[:name]
+    puts directors_database[:name][i]
     i += 1
   end
   
