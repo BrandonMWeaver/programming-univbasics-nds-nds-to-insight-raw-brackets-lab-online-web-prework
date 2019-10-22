d464cc74fd29d05d433388b5d6badb88c9e73917
Brandon Weaver <a7xncob@gmail.com>
2019-10-22 00:09:47 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -8,7 +8,14 @@ def directors_totals(nds)
   while i < directors_database.size do
     total = 0
     name = directors_database[i][:name]
+    
+    j = 0
+    while j < directors_database[i][:movies].size do
+      total += directors_database[i][:movies][j][:worldwide_gross]
+      j += 1
+    end
     i += 1
+    result += { name => total }
   end
   
   return result
